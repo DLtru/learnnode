d796f27d6050bbefbc47625d8532ccefb4fd9d76
Dimitri Larionov <dimitri.larionov@tptlive.ee>
2024-11-08 13:36:47 +0200

    Explain es vs commonjs

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-import common from "./common.js";
+//import common from "./common.js";
+//common.hello();
+//import es from "./es.js"
+//es.hello();
+
+const common = require("./common.js");
 common.hello();
-import es from "./es.js"
+const es = require("./es.js").default;
 es.hello();
